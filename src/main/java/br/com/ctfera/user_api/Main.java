7ab43b74280cec3dd357684a5a2af38fa1cb2bd9
Alisson Cavalcanti <alissoncavalcanticma@gmail.com>
2025-02-01 18:55:06 -0300

    Adjust constructor for fix this error

--- src/main/java/br/com/ctfera/user_api/Main.java
+++ src/main/java/br/com/ctfera/user_api/Main.java
@@ -1,13 +1,32 @@
 package br.com.ctfera.user_api;
 
+import br.com.ctfera.user_api.domain.User;
+import br.com.ctfera.user_api.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
-public class Main {
+public class Main implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
 	}
 
+	//Método sobrescrito da interface CommandLineRunner implementada acima
+	@Override
+	public void run(String... args) throws Exception {
+
+		userRepository.saveAll(List.of(
+				new User(null,"Alisson","alisson@gmail.com", "123", 23.20)
+		));
+
+	}
 }
