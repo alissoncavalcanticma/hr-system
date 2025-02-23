709585e98ed525b88cfcd048b1e19942538412fb
Alisson Cavalcanti <alissoncavalcanticma@gmail.com>
2025-02-23 16:58:32 -0300

    Handling ObjectNotFound Exception implementation

---
+++ src/main/java/br/com/ctfera/user_api/resources/exceptions/StandardError.java
@@ -0,0 +1,54 @@
+package br.com.ctfera.user_api.resources.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+//@Getter @Setter
+public class StandardError {
+    private LocalDateTime timestamp;
+    private String error;
+    private Integer status;
+    private String path;
+
+    public StandardError(LocalDateTime timestamp, String error, Integer status, String path) {
+        this.timestamp = timestamp;
+        this.error = error;
+        this.status = status;
+        this.path = path;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+}
--- src/main/java/br/com/ctfera/user_api/services/impl/UserServiceImpl.java
+++ src/main/java/br/com/ctfera/user_api/services/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package br.com.ctfera.user_api.services.impl;
 import br.com.ctfera.user_api.domain.User;
 import br.com.ctfera.user_api.repository.UserRepository;
 import br.com.ctfera.user_api.services.UserService;
+import br.com.ctfera.user_api.services.exceptions.ObjectNotFoundException;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -24,7 +25,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User findById(Long id) {
-        return userRepository.findById(id).orElse(null);
+        return userRepository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Object not found."));
     }
 
     @Override
