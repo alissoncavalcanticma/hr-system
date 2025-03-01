91f4eab42335aac4ff65b27f274bd379504d7ee1
Alisson Cavalcanti <alissoncavalcanticma@gmail.com>
2025-03-01 05:44:11 -0300

    Creating UserFeign.java class

--- payroll-api/src/main/java/br/com/ctfera/payroll_api/resources/PayrollResource.java
+++ payroll-api/src/main/java/br/com/ctfera/payroll_api/resources/PayrollResource.java
@@ -1,6 +1,9 @@
 package br.com.ctfera.payroll_api.resources;
 
 import br.com.ctfera.payroll_api.domain.Payroll;
+import br.com.ctfera.payroll_api.domain.User;
+import br.com.ctfera.payroll_api.feignClients.UserFeign;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -8,15 +11,23 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(value = "/api/payments")
 public class PayrollResource {
 
+    //Instancia o bean do Feign
+    @Autowired
+    private UserFeign userFeign;
+
     @GetMapping(value = "/{workerId}")
     public ResponseEntity<Payroll> getPayment(@PathVariable Long workerId, @RequestBody Payroll payment){
+
+        //Capturando o retorno da chamada do Feign
+        User user = userFeign.findById(workerId).getBody(); //getBody é necessário para garantir a captura da informação do usuário.
+
         return ResponseEntity.ok().body(
                 new Payroll(
-                        "Alisson",
+                        user.getName(),
                         payment.getDescription(),
-                        payment.getHourlyPrice(),
-                        100.0,
-                        payment.getHourlyPrice() * payment.getWorkedHours())
+                        user.getHourlyPrice(),
+                        payment.getWorkedHours(),
+                        user.getHourlyPrice() * payment.getWorkedHours())
         );
     }
 }
