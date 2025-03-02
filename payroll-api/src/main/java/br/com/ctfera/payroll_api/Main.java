f5e66dcd0dc34bce0d2c64ccd35d3967ee0d56ba
Alisson Cavalcanti <alissoncavalcanticma@gmail.com>
2025-03-02 05:07:18 -0300

    Include Annotation, pom dependency and configuration properties for this eureka client

--- payroll-api/src/main/java/br/com/ctfera/payroll_api/Main.java
+++ payroll-api/src/main/java/br/com/ctfera/payroll_api/Main.java
@@ -2,10 +2,12 @@ package br.com.ctfera.payroll_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients //Habilitando client Feign
+@EnableDiscoveryClient //Habilitando o auto-discovery do service com o Eureka
 public class Main {
 
     public static void main(String[] args) {
