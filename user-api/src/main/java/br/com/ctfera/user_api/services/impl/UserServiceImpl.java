f1391b6beb67276fb6fe36148b44a09e5507b37f
Alisson Cavalcanti <alissoncavalcanticma@gmail.com>
2025-03-04 20:39:45 -0300

    adjusts method of logging

--- user-api/src/main/java/br/com/ctfera/user_api/services/impl/UserServiceImpl.java
+++ user-api/src/main/java/br/com/ctfera/user_api/services/impl/UserServiceImpl.java
@@ -8,14 +8,19 @@ import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 
 
 import java.util.List;
 
+@ControllerAdvice
 @Slf4j
 //@NoArgsConstructor(force = true)
 @AllArgsConstructor
@@ -25,11 +30,16 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private final UserRepository userRepository;
 
-    private final Environment env;
+    //private final Environment env;
+
+    private HttpServletRequest request;
 
     @Override
     public User findById(Long id) {
-        log.info("USER_SERVICE ::: Get request on " + env.getProperty("local.server.port") + " port");
+        //log.info("USER_SERVICE ::: Get request on " + env.getProperty("local.server.port") + " port");
+
+        log.info("USER_SERVICE ::: Get request on " + request.getLocalPort()  + " port");
+
         return userRepository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Object not found."));
     }
 
